3d8e0d9974739626b0af65560bd34d0b54da91ef
EMO\peter.cooper <peter.cooper@emounlimited.com>
2016-09-08 17:41:52 +0100

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,12 +1,30 @@
 ﻿using System.Web.Mvc;
+using SimpleBlog.ViewModel;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if(!ModelState.IsValid)
+                return View(form);
+
+            //Basic Validation
+            /*if (form.Username != "test")
+            {
+                ModelState.AddModelError("Username", "Username or password isnt 20% cooler");
+                return View(form);
+            }*/
+
+            return View(form);
         }
 	}
 }
---
+++ SimpleBlog/ViewModel/Auth.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModel
+{
+    public class AuthLogin
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
