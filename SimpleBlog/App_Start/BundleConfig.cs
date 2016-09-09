6c5815f1ff71be8ef4e73de422b01e28ba7dcc64
EMO\peter.cooper <peter.cooper@emounlimited.com>
2016-09-09 14:55:04 +0100

    Bundles

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Admin bundles
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            //Frontend bundles
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/styles/jquery-3.1.0.js")
+                .Include("~/scripts/styles/jquery-validate.js")
+                .Include("~/scripts/styles/jquery-validate.unobtrusive.js")
+                .Include("~/scripts/styles/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/styles/jquery-3.1.0.js")
+                .Include("~/scripts/styles/jquery-validate.js")
+                .Include("~/scripts/styles/jquery-validate.unobtrusive.js")
+                .Include("~/scripts/styles/bootstrap.js"));
+        }
+    }
+}
